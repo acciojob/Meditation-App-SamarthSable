be09aa7ee475b32e7d6bc9439ccec439089a753f
SamarthSable <143312697+SamarthSable@users.noreply.github.com>
2026-01-18 23:04:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,26 +3,22 @@ const song = document.querySelector(".song");
 const play = document.querySelector(".play");
 const timeDisplay = document.querySelector(".time-display");
 
-const soundButtons = document.querySelectorAll(".sound-picker button");
-const timeButtons = document.querySelectorAll(".time-select button");
+song.preload = "auto";   
 
 let fakeDuration = 600;
 let remainingTime = 600;
 let timer = null;
 
-// Format time
 function formatTime(time) {
   let minutes = Math.floor(time / 60);
   let seconds = Math.floor(time % 60);
   return `${minutes}:${seconds}`;
 }
 
-// Update display
 function updateDisplay() {
   timeDisplay.textContent = formatTime(remainingTime);
 }
 
-// Start timer
 function startTimer() {
   clearInterval(timer);
 
@@ -37,32 +33,17 @@ function startTimer() {
   }, 1000);
 }
 
-// Play / Pause Button Logic (Cypress Safe)
+
 play.addEventListener("click", () => {
   if (song.paused) {
-    song.play().catch(() => {});
-    startTimer();
+    song.load();
+    song.play().then(() => {
+      startTimer();
+    }).catch(() => {});
   } else {
     song.pause();
     clearInterval(timer);
   }
 });
 
-// Change Sound
-soundButtons.forEach(button => {
-  button.addEventListener("click", function () {
-    song.src = this.getAttribute("data-sound");
-  });
-});
-
-// Change Time
-timeButtons.forEach(button => {
-  button.addEventListener("click", function () {
-    fakeDuration = parseInt(this.getAttribute("data-time"));
-    remainingTime = fakeDuration;
-    updateDisplay();
-  });
-});
-
-// Initialize
 updateDisplay();
